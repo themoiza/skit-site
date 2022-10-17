7447cc37bb3a0274a578cfff70e557bc63c37295
Moisés de Lima <the.moiza@gmail.com>
2022-10-17 10:40:56 -0300

    17/10/2022

--- public/assets/skit-dist/skit.js
+++ public/assets/skit-dist/skit.js
@@ -257,66 +257,6 @@ Debounce(() => {
 
 }, 1000, id);
 */
-window.Dialog = {
-
-	open: (obj) => {
-
-		obj.html = obj.html.split('{{dialogs}}').join('');
-
-		if(!document.getElementById('dialog')){
-
-			var dialog = document.createElement('div');
-			dialog.setAttribute('id', 'dialog');
-
-			var area = document.createElement('div');
-			area.classList.add('dialog-area');
-			area.innerHTML = obj.html;
-
-			dialog.appendChild(area);
-			document.body.appendChild(dialog);
-
-		}else{
-
-			var dialog = document.getElementById('dialog');
-			dialog.classList.remove('hidden');
-			dialog.innerHTML = '';
-
-			var area = document.createElement('div');
-			area.classList.add('dialog-area');
-			area.innerHTML = obj.html;
-
-			if(obj.close){
-				area.appendChild(c);
-			}
-
-			dialog.appendChild(area);
-
-		}
-
-		if(obj.invisible){
-			area.classList.add('dialog-invisible');
-		}
-
-		Boss.evts.add(Boss.evtTouchUp(), document.getElementById('dialog-close'), function(evts){
-
-			Boss.dialog.close();
-
-			if(obj.callBack && typeof(obj.callBack) === 'function'){
-				obj.callBack();
-			}
-		});
-	},
-	close: function(){
-
-		if(document.getElementById('dialog')){
-
-			var dialog = document.getElementById('dialog');
-			dialog.classList.add('hidden');
-			dialog.innerHTML = '';
-
-		}
-	}
-};
 window.Dualrange = {
 	unsetDisabled: (id) => {
 
@@ -584,6 +524,9 @@ class Modal{
 		this.currentY = 0;
 		this.comp.style.transform = null;
 		this.comp.style.zIndex = null;
+	}
+	done(fn){
+		fn();
 	}
 }
 class Tabs{
